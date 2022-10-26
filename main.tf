cf55ec8b8cb73fa1b3a8a35064efb41a2e90a666
Dias Saparov <dias@digger.dev>
2022-10-26 15:17:45 +0100

    remove aws provider

--- main.tf
+++ main.tf
@@ -1,17 +1,5 @@
 terraform {
   required_version = ">= 0.12"
-
-  required_providers {
-    aws = {
-      source  = "aws"
-      version = "~> 4.0"
-    }
-  }
 }
-
-provider "aws" {
-  region  = var.region
-}
-
 
 
